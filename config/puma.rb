6888f6e81747c1b39bc0f071d0179213d4ee0bcc
ppatel101 <parth.logicalstreet@gmail.com>
2024-02-02 23:58:28 +0530

    remove puma doeamon

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,3 @@
-require 'puma/daemon'
-
 # This configuration file will be evaluated by Puma. The top-level methods that
 # are invoked here are part of Puma's configuration DSL. For more information
 # about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
@@ -35,5 +33,3 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-
-daemonize
